1f80cf31aa76d5793ba7639f5a1d532cef4c5b93
Crowdbotics <engineering@crowdbotics.com>
2023-04-25 00:21:42 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "square-voice-dev-78767",
+  url: "https://square-voice-dev-78767.botics.co",
+  api: "https://square-voice-dev-78767.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
